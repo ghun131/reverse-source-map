380bb2b01a0c847df3380fa18c3295d318417585
ghun131 <hungdh131@gmail.com>
2022-02-28 15:40:39 +0700

    video instruction

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # What does it do?
 
 It take in the production built `.js.map` file and `js`, then it reverses back to the orignal typescript file named `empty.ts`
+
+**Video instruction**: https://watch.screencastify.com/v/HRbJ3O66xDe6InJQk1RH
--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const { SourceMapConsumer } = require("source-map");
 
 // webpack source-map
 
-fs.readFile("./_app-a4160044322d8d42.js.map", "utf8", async (error, data) => {
+fs.readFile("./6159-4a2ea98f603d0161.js.map", "utf8", async (error, data) => {
   if (error) return console.error(error);
 
   const parsed = await SourceMapConsumer.with(data, null, (consumer) => {
